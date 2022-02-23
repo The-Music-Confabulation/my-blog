e70bb86aa1fab6605989c7eb79264e0e75496bbf
Thanh Nam Tran <ttran19@umbc.edu>
2022-02-23 14:18:04 -0500

    bug fix

--- app.js
+++ app.js
@@ -207,15 +207,15 @@ app.get("/posts/:postId", (req, res) =>{
 
 
 //for debug local
-app.listen(3000, function() {
-  console.log("Server started");
-});
-
-// let port = process.env.PORT;
-// if (port == null || port == "") {
-//   port = 3000;
-// }
-
-// app.listen(port, function() {
+// app.listen(3000, function() {
 //   console.log("Server started");
 // });
+
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+app.listen(port, function() {
+  console.log("Server started");
+});
