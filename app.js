432b805da44e3e043019f22dfce511926c39804e
Thanh Nam Tran <ttran19@umbc.edu>
2022-02-24 00:24:41 -0500

    user auth, log in and out session, cookie setup

--- app.js
+++ app.js
@@ -1,29 +1,57 @@
-//jshint esversion:6
+//dotenv config
 require('dotenv').config();
+
+//nodejs setup
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 
-// Load the full build.
-const _ = require('lodash');
+//passport for auth
+const session = require('express-session');
+const passport = require('passport');
+const LocalStrategy = require('passport-local')
+const passportLocalMongoose = require("passport-local-mongoose");
+
+//password auth
+const encrypt = require("mongoose-encryption");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+
+//mongodb plugin
 const mongoose = require("mongoose");
 
-const encrypt = require("mongoose-encryption");
 
+//lodash for url processing
+const _ = require('lodash');
 
+//App Set Up
 const app = express();
+app.use(express.static("public"));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
-var posts = [];
+
+app.use(session({
+  secret: process.env.SECRET,
+  resave: false,
+  saveUninitialized: true,
+  cookie: {}
+}))
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+
 
 var pass = process.env.MONGOPW;
-
-mongoose.connect("mongodb+srv://ttran293:" + 
-                                      pass + 
-                "@cluster0.1wmqh.mongodb.net/blog_content?retryWrites=true&w=majority", 
-                {useNewUrlParser: true});
+mongoose.connect("mongodb+srv://ttran293:" + pass + "@cluster0.1wmqh.mongodb.net/blog_content?retryWrites=true&w=majority", {
+  useNewUrlParser: true
+});
 
 
-// mongoose.connect("mongodb://localhost:27017/blog_content",{useNewUrlParser:true});
+var posts = [];
 
 
 const page = {
@@ -33,9 +61,9 @@ const page = {
   // month: "number",
   // year: "number",
   title: "string",
-  content : "string",
+  content: "string",
   // author : "string",
-  url : "string"
+  url: "string"
 };
 
 
@@ -46,36 +74,45 @@ const Post = mongoose.model(
 
 
 const userSchema = new mongoose.Schema({
-  user_name :"string",
+  username: "string",
   // name : "string",
-  password : "string",
-  email : "string"
+  password: "string",
+  email: "string"
 });
 
-
-userSchema.plugin(encrypt, {secret: process.env.SECRET , encryptedFields: ["password"] } );
-
+userSchema.plugin(passportLocalMongoose);
 
 const User = new mongoose.model(
-  "User", userSchema 
+  "User", userSchema
 );
+
+
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 
 
 
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
 app.use(express.static("public"));
 
+let isLoggedIn = false;
 
 //use nodemon app.js
-app.get("/",async function(req, res){
+app.get("/", async function (req, res) {
 
   var perPage = 3; //limit how many songs per page
   var total = await Post.count();
   // console.log(total);
 
-  var pages = Math.ceil(total/perPage);//calculate how many pages needed
+  var pages = Math.ceil(total / perPage); //calculate how many pages needed
   //console.log(pages)
   var pageNumber = (req.query.page == null) ? 1 : req.query.page;
 
@@ -84,91 +121,109 @@ app.get("/",async function(req, res){
   var songs = await Post.find({}).skip(startFrom).limit(perPage);
 
   //console.log(songs.length)
-  Post.find({}, function(err, foundItems){
-      res.render("home",{
-        newListItems: foundItems,
-        pages:pages,
-        songs:songs
-      });
+  Post.find({}, function (err, foundItems) {
+    res.render("home", {
+      newListItems: foundItems,
+      pages: pages,
+      songs: songs,
+      isLoggedIn: isLoggedIn
+    });
   });
-
-
-
 });
 
-app.get("/help",function(req, res){
-  res.render("help");
+app.get("/help", function (req, res) {
+  res.render("help", {isLoggedIn: isLoggedIn});
 });
 
 
-app.get("/signup",function(req, res){
-  res.render("signup");
+app.get("/signup", function (req, res) {
+  res.render("signup", {isLoggedIn: isLoggedIn});
 });
 
 
-app.post('/signup', (req, res) => {
 
-  const client = new User ({
-    user_name: req.body.userName,
-    password: req.body.userPassword,
-    email: req.body.userEmail
-  });
+//https://stackoverflow.com/questions/48096378/bad-request-when-registering-with-passport
+//passport.authenticate is a middleware, which means that you have to call it with 3 parameters (req, res, next)
+//without 3 parameters it returns bad request error
+app.post("/signup", function (req, res, next) {
+  // console.log(req.body.username);
+  // console.log(req.body.userpassword);
+    var newUser = new User({
+        username: req.body.username
+    });
 
-  client.save(function(err){
-    if (!err){
-      res.redirect("/");
-    }
-    else {
-      console.log(err);
-    }
-  })
-})
+    //need to check if user already exists here
+
+
+    
+    User.register(newUser, req.body.userpassword, function (err, user) {
+        if (err) {
+            console.log(err);
+            return res.redirect('/signup');
+        }
+        // go to the next middleware
+        next();
+    });
+}, passport.authenticate('local', { 
+    successRedirect: '/compose',
+    failureRedirect: '/login' 
+}
+)
+);
 
 app.get('/login', (req, res) => {
-  res.render("login");
+  res.render("login", {isLoggedIn: isLoggedIn});
 })
 
 
-app.post("/login",function(req, res){
-  const login_username = req.body.username;
-  const login_password = req.body.password;
+app.post('/login',function(req, res){
 
-  User.findOne({user_name : login_username}, function(err, foundUser){
-    if (err){
+  // passport.authenticate('local', { failureRedirect: '/login' }),
+  // function(req, res) {
+  //   res.redirect('/compose');
+
+
+  const user = new User({
+    username: req.body.username,
+    password: req.body.password
+  });
+
+  req.login(user, function(err){
+    if (err) {
       console.log(err);
+    } else {
+      passport.authenticate("local")(req, res, function(){
+        isLoggedIn = true;
+        res.redirect("/compose");
+      });
+      // passport.authenticate("local", {failureRedirect: '/login'}),
+      // function(req, res){
+      //   isLoggedIn = true;
+      //   res.redirect("/compose");
+      // }
     }
-    else {
-      if (foundUser) {
-        console.log("user found");
-        if(foundUser.password === login_password){
-          console.log("password matched");
-          res.render("compose");
-        }
-        else{
-          console.log("password not match");
-        }
-
-      }
-    }
-  })
-  
+  });
 });
 
-app.get("/compose",function(req, res){
-  res.render("compose");
+app.get("/compose", function (req, res) {
+  if (req.isAuthenticated()) {
+    res.render("compose", {isLoggedIn: isLoggedIn});
+  } else {
+    res.redirect("/login");
+  }
 });
 
 
 app.post('/compose', (req, res) => {
 
-  const post = new Post ({
+  const post = new Post({
     title: req.body.postTitle,
     content: req.body.postBody,
     url: req.body.postURL
   });
 
-  post.save(function(err){
-    if (!err){
+  post.save(function (err) {
+    if (!err) {
       res.redirect("/");
     }
   })
@@ -177,12 +232,19 @@ app.post('/compose', (req, res) => {
 
 })
 
+app.get('/logout', (req, res) => {
+  req.logout();
+  isLoggedIn = false;
+  res.redirect("/");
+})
 
-app.get("/posts/:postId", (req, res) =>{
+
+
+app.get("/posts/:postId", (req, res) => {
 
 
   const topic_title = _.lowerCase(req.params.postId)
-  
+
   // posts.forEach(element => {
   //   const post_header = _.lowerCase( element.title )
   //   const post_url = _.kebabCase(element.title)
@@ -191,12 +253,14 @@ app.get("/posts/:postId", (req, res) =>{
   //         res.render("post", {
   //           pageTitle : element.title,
   //           pageContent : element.content
-  //         })
+  //         })y
   //     }
   // });
 
   const requestedPostId = req.params.postId;
-  Post.findOne({_id: requestedPostId}, function(err, post){
+  Post.findOne({
+    _id: requestedPostId
+  }, function (err, post) {
     res.render("post", {
       title: post.title,
       content: post.content
@@ -205,7 +269,9 @@ app.get("/posts/:postId", (req, res) =>{
 
 })
 
-
+app.get('/test', (req, res) => {
+  res.render('test');
+})
 //for debug local
 // app.listen(3000, function() {
 //   console.log("Server started");
@@ -216,6 +282,6 @@ if (port == null || port == "") {
   port = 3000;
 }
 
-app.listen(port, function() {
+app.listen(port, function () {
   console.log("Server started");
 });
