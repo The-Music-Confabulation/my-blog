f282e34e33192fabcbef8a69d5d31bb47077562d
Thanh Nam Tran <=>
2022-03-23 11:29:49 -0400

    allow comment at homepage

--- public/scripts/scripts.js
+++ public/scripts/scripts.js
@@ -87,7 +87,3 @@ function updateFollowing(username, user_id){
   ajax.send();
 }
 
-var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
-var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-  return new bootstrap.Tooltip(tooltipTriggerEl)
-})
